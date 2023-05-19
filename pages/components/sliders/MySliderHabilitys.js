cef2115b100826c99ed34a9f9bd1b01214271e71
Martin Camer <martincamer011@gmail.com>
2023-05-19 17:43:30 -0300

    card slider

--- pages/components/sliders/MySliderHabilitys.js
+++ pages/components/sliders/MySliderHabilitys.js
@@ -1,5 +1,5 @@
+// import CardSlider from './CardSlider';
 import Slider from 'react-slick/lib/slider';
-import CardSlider from './CardSlider';
 import { IoLogoJavascript } from 'react-icons/io';
 import {
 	SiCss3,
@@ -128,10 +128,24 @@ function MySliderHabilitys() {
 		<div className={'relative pt-32'}>
 			<Slider {...settings}>
 				{habilidades.map(index => (
-					<CardSlider
+					<div
 						key={index.id}
-						index={index}
-					/>
+						className={
+							'flex flex-col justify-center items-center gap-2 bg-six h-[100%] w-[100%] md:w-full md:h-full rounded-3xl py-12 px-9 max-md:py-8 max-md:px-3'
+						}
+					>
+						<div className="flex items-center justify-center text-3xl text-secondary bg-terciary rounded-full md:w-20 max-md:h-[4rem] max-md:w-[4rem] md:h-20">
+							{index.image}
+						</div>
+						<h3
+							className={
+								'font-bold mt-6 mb-3 md:text-2xl max-md:text-1xl text-secondary'
+							}
+						>
+							{index.title}
+						</h3>
+						<div className="h-1.5 w-14 max-md:h-0.5 bg-secondary"></div>
+					</div>
 				))}
 			</Slider>
 		</div>
